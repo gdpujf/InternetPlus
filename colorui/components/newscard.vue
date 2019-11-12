14a1a52f672d56c9179bd345d96bb8c82636d4b2
qqggwm <936450937@qq.com>
2019-11-12 18:21:21 +0800

    新功能:首页增加了新闻浏览功能 新增:引入了colorui-card模块
    Signed-off-by: qqggwm <936450937@qq.com>

---
+++ colorui/components/newscard.vue
@@ -0,0 +1,54 @@
+<template>
+	<view >
+		<cu-custom bgColor="bg-gradual-pink" :isBack="false"><block slot="content">首页</block></cu-custom>
+		<view class="cu-bar bg-white solid-bottom">
+			
+			<view class="action">
+				<switch :class="isCard?'checked':''" :checked="isCard?true:false" @change="IsCard"></switch>
+			</view>
+		</view>
+		
+		<view v-for="(item,index) in reciveUserInfo" :key="index" class="cu-card case" :class="isCard?'no-card':''" >
+			<view class="cu-item shadow">
+				<view class="image">
+					<image src="https://ossweb-img.qq.com/images/lol/web201310/skin/big10006.jpg"
+					 mode="widthFix"></image>
+					<view class="cu-tag bg-blue">{{item.type}}</view>
+					<view class="cu-bar bg-shadeBottom"> <text class="text-cut">{{item.text}}</text></view>
+				</view>
+				<view class="cu-list menu-avatar">
+					<view class="cu-item">
+						<view class="cu-avatar round lg" style="background-image:url(https://ossweb-img.qq.com/images/lol/web201310/skin/big10006.jpg);"></view>
+						<view class="content flex-sub">
+							<view class="text-grey">{{item.author}}</view>
+							<view class="text-gray text-sm flex justify-between">
+								{{item.time}}
+								<view class="text-gray text-sm">
+									<text class="cuIcon-attentionfill margin-lr-xs"></text> {{item.view}}
+									<text class="cuIcon-appreciatefill margin-lr-xs"></text> {{item.like}}
+									<text class="cuIcon-messagefill margin-lr-xs"></text> {{item.remark}}
+								</view>
+							</view>
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+<script>
+	export default {
+		props:["reciveUserInfo"],
+		data() {
+			
+			return {
+				isCard: false,			
+			};
+		},
+		methods: {
+			IsCard(e) {
+				this.isCard = e.detail.value
+			},
+		}
+	}
+</script>
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,63 +1,33 @@
 <template name="basics">
 	<view>
 		<scroll-view scroll-y class="page">
-			<image src="/static/componentBg.png" mode="widthFix" class="response"></image>
-			 <view class="solids-bottom padding-xs flex align-center">
-			 	<view class="flex-sub text-center">
-			 		<view class="solid-bottom text-lg padding response" mode="widthFix">
-			 			<text class="text-black">每日一句：xxxx</text>
-			 		</view>
-			 	</view>
-			 </view>
-	
-			<view class="nav-list">
-				<navigator hover-class="none" :url="'/pages/basics/' + item.name" class="nav-li" navigateTo :class="'bg-'+item.color"
-				 :style="[{animation: 'show ' + ((index+1)*0.2+1) + 's 1'}]" v-for="(item,index) in elements" :key="index">
-					<view class="nav-title">{{item.title}}</view>
-					<view class="nav-name">{{item.name}}</view>
-					<text :class="'cuIcon-' + item.cuIcon"></text>
-				</navigator>
+			<view class="newscard" >
+				<newscard :reciveUserInfo="lists">				
+				</newscard>
 			</view>
+			
+			
 			<view class="cu-tabbar-height"></view>
 		</scroll-view>
 	</view>
 </template>
 
 <script>
+	import newscard from "../../colorui/components/newscard.vue";  
 	export default {
-		name: "basics",
+		 name: "basics",
+		components:{
+				newscard
+		},
 		data() {
 			return {
-				elements: [{
-						title: '功能1',
-						name: 'layout',
-						color: 'cyan',
-						cuIcon: 'newsfill'
-					},
-					{
-						title: '功能2',
-						name: 'background',
-						color: 'blue',
-						cuIcon: 'colorlens'
-					},
-					{
-						title: '功能3',
-						name: 'text',
-						color: 'purple',
-						cuIcon: 'font'
-					},
-					{
-						title: '功能4',
-						name: 'icon',
-						color: 'mauve',
-						cuIcon: 'cuIcon'
-					},
-				],
-			};
+				lists:[
+					{type:"文章","text":'我是内容部分',"author":"weiming",time:"今天","view":'9999+',"like":20,"remark":30},
+					{type:"文章","text":'我是内容部分',"author":"weiming",time:"今天","view":'9999+',"like":20,"remark":30},
+					{type:"文章","text":'我是内容部分',"author":"weiming",time:"今天","view":'9999+',"like":20,"remark":30},
+					]
+			}
 		},
-		onShow() {
-			console.log("success")
-		}
 	}
 </script>
 
